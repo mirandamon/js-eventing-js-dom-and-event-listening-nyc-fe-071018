c897f87332fb2fd2b7891ab39f32623f8c3ab6fe
Nathan Miranda <nate845@gmail.com>
2018-08-21 23:44:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ function bubble(event) {
 }
 
 for (div of divs) {
-  div.addEventListener('click', bubble, false)
+  div.addEventListener('click', bubble)
 }
 
-/*
 shoppingPage.addEventListener('click', (event) => {
   console.log(event)
   alert("Hey you clicked me!")
-})*/
-/*
+})
+
 paragraph.addEventListener('click', (event) => {
   paragraph.style.backgroundColor = 'yellow'
   alert("You clicked the p!!!")
-})*/
+})

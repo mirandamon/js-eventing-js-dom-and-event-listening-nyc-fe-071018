fcee54a3befbd7ff78b254221ced081185da6476
Nathan Miranda <nate845@gmail.com>
2018-08-21 23:34:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const shoppingPage = document.querySelector('#main')
 const paragraph = document.querySelector('p')
 
+let divs = document.querySelectorAll('div')
+
+/*
 shoppingPage.addEventListener('click', (event) => {
   console.log(event)
   alert("Hey you clicked me!")
-})
-
+})*/
+/*
 paragraph.addEventListener('click', (event) => {
   paragraph.style.backgroundColor = 'yellow'
   alert("You clicked the p!!!")
-})
+})*/

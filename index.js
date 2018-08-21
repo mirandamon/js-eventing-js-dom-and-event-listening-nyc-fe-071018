57d772539680ca3ddffce227e8cf21c766cca26d
Nathan Miranda <nate845@gmail.com>
2018-08-21 23:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const main = document.querySelector('#main')
 const paragraph = document.querySelector('p')
 
 main.addEventListener('click', (event) => {
-  alert("Hey you clicked me!")
+  //alert("Hey you clicked me!")
 })
 
 paragraph.addEventListener('click', (event) => {
   paragraph.style.backgroundColor = 'yellow'
-  alert("You clicked the p!!!")
+  console.log('hi')
 })

6c7d9dd3f72d2148ccad71a6c54695fc10c63719
Nathan Miranda <nate845@gmail.com>
2018-08-21 23:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const paragraph = document.querySelector('p')
 main.addEventListener('click', (event) => {
   alert("Hey you clicked me!")
 })
+
+paragraph.addEventListener('click', (event) => {
+  alert("You clicked the p!!!")
+})
